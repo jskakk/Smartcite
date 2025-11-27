5101665af66212afc8121fd59ca8aef137517a1a
jskakk <basejascha@gmail.com>
2025-11-27 20:32:28 +0800

    Smartcite brief description

---
+++ src/main/java/com/appdevg6/smartcite/BaseraisinsApplication.java
@@ -0,0 +1,13 @@
+package com.appdevg6.smartcite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseraisinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseraisinsApplication.class, args);
+	}
+
+}
